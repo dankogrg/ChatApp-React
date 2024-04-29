c83b946b737bdd3a8b61e660f3e576aa1f08897f
dankogrg <dankogrg@gmail.com>
2024-04-29 14:48:25 +0200

    userColor set as required

--- src/fragments/CreateUser.tsx
+++ src/fragments/CreateUser.tsx
@@ -3,16 +3,27 @@ import { Button, FloatingLabel, Form, Modal } from 'react-bootstrap';
 
 const CreateUser = ({ shownModal, hideModal, makeUser }: any) => {
     const [userName, setUserName] = useState('');
-    const [color, setColor] = useState('#0000ff');
+    const [color, setColor] = useState('#ff0000');
     const date: string = String(
         parseInt(String(new Date().getTime() * Math.random())),
     );
 
+    const [colorStatus, setColorStatus] = useState('empty');
+    const [warning, setWarning] = useState('');
+
     const handleUserName = (e: any) => setUserName(e.target.value);
-    const handleColor = (e: any) => setColor(e.target.value);
+    const handleColor = (e: any) => {
+        setColor(e.target.value);
+        setColorStatus('full');
+        console.log(colorStatus);
+    };
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
+        if (colorStatus == 'empty') {
+            setWarning('Please pick a background color!');
+            return;
+        }
         const member = {
             userName: userName,
             color: color,
@@ -31,7 +42,7 @@ const CreateUser = ({ shownModal, hideModal, makeUser }: any) => {
                 <Modal.Body>
                     <FloatingLabel
                         controlId="floatingUserName"
-                        label="Username"
+                        label="* Username"
                         className="mb-3"
                     >
                         <Form.Control
@@ -43,14 +54,26 @@ const CreateUser = ({ shownModal, hideModal, makeUser }: any) => {
                         />
                     </FloatingLabel>
                     <Form.Label id="color" className="mb-3">
-                        Choose color
+                        <b>* Choose color</b>
                     </Form.Label>
-                    <Form.Control
-                        type="color"
-                        className="mb-3"
-                        onChange={handleColor}
-                        value={color}
-                    />
+                    <div style={{ display: 'flex', verticalAlign: 'middle' }}>
+                        <Form.Control
+                            type="color"
+                            className="form-control form-control-color"
+                            onChange={handleColor}
+                            value={color}
+                            required
+                        />
+                        <p
+                            style={{
+                                color: '#ff0000',
+                                margin: 'auto ',
+                                marginLeft: '7px',
+                            }}
+                        >
+                            {warning}
+                        </p>
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="primary" type="submit">
